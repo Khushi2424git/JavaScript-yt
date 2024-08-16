2dbd24a09c723b1155dba9342f5f30ce865ed4a1
Khushi2424git <khushisudh96@gmail.com>
2024-08-16 06:07:17 +0000

    comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,34 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null < 0)
+console.log(null >= 0)
+console.log(null <= 0)
+console.log(null == 0)
+
+console.log(null > 0)
+console.log(null < 0)
+console.log(null >= 0)
+console.log(null <= 0)
+console.log(null == 0)
+// Note -
+// The reason is that an equality check == and comparisons >, <, >=, <= works differently.
+// comparisons convert null to a number, testing is as 0.
+// that's why (3) null >= 0 is true and(1) null > 0 is false.
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+// console.log(undefined == 0)
+ 
+// === => Identical operator
+console.log("2" === 2) //checks data type and values at same time 
