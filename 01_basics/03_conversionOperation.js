5f49ef26e25a42c16f94700c9040908b247bf257
Khushi2424git <khushisudh96@gmail.com>
2024-08-16 05:26:53 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "khushi"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // Notes ->
 // "43" => 43
@@ -15,7 +15,7 @@ console.log(valueInNumber)
 
 let isLoggedIn = "khushi"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // Notes -
 // 1 => true; 0 => false
@@ -24,5 +24,12 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 43
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+// *************************** Operations ***************************
+
+let value = 6
+let negValue = -value
+console.log(negValue)
