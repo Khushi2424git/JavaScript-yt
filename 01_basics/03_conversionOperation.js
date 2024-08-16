8765161b8b006e1df9179ee52f6c2c7ea37b6416
Khushi2424git <khushisudh96@gmail.com>
2024-08-16 05:46:35 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,4 +32,36 @@ let stringNumber = String(someNumber)
 
 let value = 6
 let negValue = -value
-console.log(negValue)
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+let str1 = "Hello "
+let str2 = "Khushi"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 +2 + "3")
+// console.log((3+4)*54)
+
+// console.log(true)
+// console.log(+true)
+// console.log(true+) //gives error
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
+
