966e70b9f8e66ef6a18c45e8e4ae8269f78d965a
Khushi2424git <khushisudh96@gmail.com>
2024-08-16 05:19:38 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "khushi"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// Notes ->
+// "43" => 43
+// "45abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "khushi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// Notes -
+// 1 => true; 0 => false
+// "" => false
+// "khushi" => true
+
+let someNumber = 43
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
