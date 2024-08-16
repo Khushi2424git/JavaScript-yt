c0503c247efac958bc75f67282ca8187f62bbb00
Khushi2424git <khushisudh96@gmail.com>
2024-08-16 06:25:48 +0000

    dataTypes-summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,18 @@
+// Primitive 
+// 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+const bigNumber = 343558482678429108022497n
+
+// Reference (Non- Primitive)
+Array, Object, Function
