9ddba8ae2fa7c0d226e8d768a8d06385fbdb492d
Khushi2424git <khushisudh96@gmail.com>
2024-08-16 05:00:08 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(45 + 87); //we are using node js not browser
+
+let username  = "khushi"
+let age = 20
+
+
+// Primitive data types
+// number => 2 to 53
+// BigInt
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => 
+// Symbol =>unique
+
+// Non- Primitive data types
+// object =>
+
+console.log(typeof "khushi")
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
